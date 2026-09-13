74da4e44cec812ad32c89783b6321b7819c4f0b2
priyanka2002sept-pixel <priyanka2002sept@gmail.com>
2026-09-13 17:33:24 +0530

    Delete AWS-Ecommerce-Platform directory

--- AWS-Ecommerce-Platform/terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "products_table_name" { value = aws_dynamodb_table.products.name }
-output "orders_table_name"   { value = aws_dynamodb_table.orders.name }
-output "user_pool_id"        { value = aws_cognito_user_pool.main.id }
-output "user_pool_client_id" { value = aws_cognito_user_pool_client.main.id }
-output "cloudfront_url"      { value = "https://${aws_cloudfront_distribution.frontend.domain_name}" }
--- AWS-Ecommerce-Platform/terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_region"   { type = string; default = "us-east-1" }
-variable "environment"  { type = string; default = "dev" }
-variable "project_name" { type = string; default = "serverless-ecommerce" }
-variable "alert_email"  { type = string; default = "YOUR_EMAIL_ADDRESS" }
